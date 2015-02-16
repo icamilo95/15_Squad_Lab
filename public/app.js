0b498ddee99eb46bfa0e342cc47d9b056c06441f
Camilo Ordonez <icoj@gmail.com>
2015-02-16 15:49:55 -0800

    about to add JS

---
+++ public/app.js
@@ -0,0 +1,31 @@
+$(document).ready(function() { 
+
+
+
+function hiding() {
+
+   $('.test').on("click", function(){
+      alert("CAmilo");
+      $('.editSquad').fadeOut(3000);
+
+   });
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+hiding();
+ });
+
+
+
